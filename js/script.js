69cf95d5c395dc47f5a0a80e3a58739ea4f643b1
Haakonjohansenn <111745800+Haakonjohansenn@users.noreply.github.com>
2023-01-05 09:50:01 +0100

    js training

---
+++ js/script.js
@@ -0,0 +1,21 @@
+var nubmer1 = 4;
+var number2 = 4.5;
+
+let weekdays = "monday" + "-" + "tuesday" + "-" + "wednesday" + "-" + "thursday" + "-" + "friday";
+console.log(weekdays);
+
+var addition =(number1 + number2);
+var subtraction =(number1 + number2);
+var multiplication =(number1 + number2);
+var division =(number1 + number2);
+
+var isLoggedIn = false;
+
+console.log(isLoggedIn);
+
+var timeElapsed;
+var totalPrice;
+
+var numberOfCharacters = "The frogs are angry."
+console.log(numberOfCharacters.length);
+
